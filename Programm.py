d9995799b76f56c6b4497299a4407770315d4dba
Tamir Yurovskiy <1hotlev@gmail.com>
2021-06-09 22:12:37 +0300

    comments added in Programm.py

--- Programm.py
+++ Programm.py
@@ -5,17 +5,32 @@ from helpers import generate_random_numbers_string
 class Programm:
 
     @staticmethod
-    def get_passwords():
+    def get_passwords() -> object:
+        """
+        Return all rows from passwords data base
+        :return: object - rows
+        """
         passwords = DBConnection.fetch_records("SELECT * FROM passwords")
         return passwords
 
     @staticmethod
     def save_password(password: str) -> bool:
+        """
+        Insert password to passwords data base
+        :param password:
+        :return: true if success otherwise false
+        """
         query = "INSERT INTO passwords (password) VALUES(%s)"
         return DBConnection.insert(query, (password))
 
     @staticmethod
-    def generate_password(path: str, number: int) -> bool:
+    def generate_password(path: str, number: int) -> str:
+        """
+        Build string of passwords.
+        :param path:
+        :param number:
+        :return: string - password
+        """
         password = ""
         for i in range(number):
             rand_line = generate_random_numbers_string()
@@ -25,11 +40,23 @@ class Programm:
 
     @staticmethod
     def make_password(path: str, number: int) -> bool:
+        """
+        Build password and save it to data base
+        :param path:
+        :param number:
+        :return: true if success otherwise false
+        """
         password = Programm.generate_password(path, number)
         return Programm.save_password(password)
 
     @staticmethod
     def find_string_by_number(number, path) -> str:
+        """
+        Search right string in file by given number
+        :param number:
+        :param path:
+        :return: string with sub-password
+        """
         with open(path, 'r') as f:
             line = f.readline()
             while line:
